94fc64b6730f74a2bab05b1672c68908c1347250
Harysson Soares <haryssonsoares@gmail.com>
2021-04-02 13:04:53 -0300

    fix: parametters employee change cpf to registration

--- app/Controllers/Http/Employee/EmployeeController.js
+++ app/Controllers/Http/Employee/EmployeeController.js
@@ -1,171 +1,159 @@
-'use strict'
+"use strict";
 
 const Hash = use("App/Models/Hash");
 const sendEmail = use("./../../../helpers/sendEmail");
-const Database = use("Database")
-const User = use("App/Models/User")
-
+const Database = use("Database");
+const User = use("App/Models/User");
 
 class EmployeeController {
+  async store({ request, response }) {
+    const { username, email, cpf, photo, registration } = request.all();
 
-    async store ({ request, response }) {
+    const trx = await Database.beginTransaction();
 
-        const { username, email, cpf, photo, registration } = request.all();
-    
-        const trx = await Database.beginTransaction();
-    
-        try {
-    
-          const user = await User.create({
-            username,
-            email,
-            cpf, 
-            photo, 
-            registration
-          }, trx);
-    
-          const confirmation_token = Math.random().toString(36).slice(5);
-    
-          const hash = await Hash.create({
-            user_id: user.id,
-            confirmation_token 
-          }, trx)
-    
-          await sendEmail({code: confirmation_token, email: user.email, username: user.username})
-    
-          trx.commit();
-    
-          return response.status(200).json({user});
-        
-        } catch (err) {
-          trx.rollback();
-          console.log(err);
-          return response.status(400).json({
-            message: "error creating employee",
-          });
-        }
+    try {
+      const user = await User.create(
+        {
+          username,
+          email,
+          registration,
+          photo,
+        },
+        trx
+      );
+
+      const confirmation_token = Math.random().toString(36).slice(5);
+
+      const hash = await Hash.create(
+        {
+          user_id: user.id,
+          confirmation_token,
+        },
+        trx
+      );
+
+      await sendEmail({
+        code: confirmation_token,
+        email: user.email,
+        username: user.username,
+      });
+
+      trx.commit();
+
+      return response.status(200).json({ user });
+    } catch (err) {
+      trx.rollback();
+      console.log(err);
+      return response.status(400).json({
+        message: "error creating employee",
+      });
+    }
+  }
+
+  async show({ params, request, response, view }) {
+    try {
+      const user = await User.findOrFail(params.id);
+
+      return response.status(200).json(user);
+    } catch (err) {
+      return response.status(404).json({
+        message: "error showing user",
+      });
+    }
+  }
+
+  async update({ params, request, response }) {
+    const data = request.all();
+
+    try {
+      const user = await User.findOrFail(params.id);
+
+      if (!user) {
+        return response.status(404).json({
+          message: "error updating user",
+        });
       }
 
+      user.merge(data);
 
-      async show ({ params, request, response, view }) {
+      await user.save();
 
-        try {
-          const user = await User.findOrFail(params.id);
-    
-          return response.status(200).json(user);
-        } catch (err) {
-          return response.status(404).json({
-            message: "error showing user",
-          });
-        }
-    
+      return response.status(200).json({
+        message: "user updated",
+        user,
+      });
+    } catch (err) {
+      console.error(err);
+
+      return response.status(404).json({
+        message: "error updating user",
+      });
+    }
+  }
+
+  async destroy({ params, request, response }) {
+    try {
+      const user = await User.findOrFail(params.id);
+
+      if (!user) {
+        return response.status(404).json({
+          message: "user not found",
+        });
       }
 
-      async update ({ params, request, response }) {
+      user.merge({
+        deleted_at: new Date(),
+      });
 
-        const data = request.all();
-    
-        try {
-          const user = await User.findOrFail(params.id);
-    
-          if (!user) {
-            return response.status(404).json({
-              message: "error updating user",
-            });
-          }
-    
-          user.merge(data);
-    
-          await user.save();
-    
-          return response.status(200).json({
-            message: "user updated",
-            user,
-          });
-        } catch (err) {
-          console.error(err);
-    
-          return response.status(404).json({
-            message: "error updating user",
-          });
-        }
-    
+      await user.save();
+
+      return response.status(200).json({
+        message: "user deleted",
+      });
+    } catch (err) {
+      console.log(err);
+
+      return response.status(400).json({
+        message: "error deleting user",
+      });
+    }
+  }
+
+  async passwordConfirmation({ request, response }) {
+    const { password, confirm_password } = request.all();
+    const { token } = request.get();
+    try {
+      if (password != confirm_password) {
+        return response.status(400).json({
+          message: "password is different",
+        });
       }
 
-      async destroy ({ params, request, response }) {
+      const hash = await Hash.query()
+        .where("confirmation_token", token)
+        .first();
 
-        try {
-          const user = await User.findOrFail(params.id);
-    
-          if (!user) {
-            return response.status(404).json({
-              message: "user not found",
-            });
-          }
-    
-          user.merge({
-            deleted_at: new Date()
-          });
-    
-          await user.save();
-    
-          return response.status(200).json({
-            message: "user deleted",
-          });
-        } catch (err) {
-          console.log(err);
-    
-          return response.status(400).json({
-            message: "error deleting user",
-          });
-        }
-    
-      }
+      console.log("HASH", hash, hash.user_id);
 
+      const user = await User.findBy("id", hash.user_id);
 
-      async passwordConfirmation({request, response}) {
-        const  { password, confirm_password } = request.all()
-        const { token } = request.get()
-        try {
-          
-          if (password != confirm_password) {
-            return response.status(400).json({
-              message: "password is different"
-            })
-          }
-      
-          const hash = await Hash.query()
-          .where("confirmation_token", token)
-          .first(); 
+      user.password = password;
 
-          console.log("HASH",hash, hash.user_id)
-      
-          const user = await User.findBy("id", hash.user_id);
-      
-          user.password =  password; 
-          
-          await user.save();
-          
-          await hash.delete(); 
-  
-          return response.status(200).json({
-            message: "password confirmed"
-          })
+      await user.save();
 
-        } catch (err) {
+      await hash.delete();
 
-          console.log(err); 
+      return response.status(200).json({
+        message: "password confirmed",
+      });
+    } catch (err) {
+      console.log(err);
 
-          return response.status(200).json({
-            message: "error confirmating password"
-          })
-
-        }
-    
-      }
-
-
-
+      return response.status(200).json({
+        message: "error confirmating password",
+      });
+    }
+  }
 }
 
-module.exports = EmployeeController
+module.exports = EmployeeController;
