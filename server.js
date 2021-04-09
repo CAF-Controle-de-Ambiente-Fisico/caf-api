6f32a35f8bed9a7a491c5a215797195cee489f70
Harysson Soares <haryssonsoares@gmail.com>
2021-04-09 19:43:27 -0300

    ssl

--- server.js
+++ server.js
@@ -1,40 +1,17 @@
-'use strict'
-
-/*
-|--------------------------------------------------------------------------
-| Http server
-|--------------------------------------------------------------------------
-|
-| This file bootstraps Adonisjs to start the HTTP server. You are free to
-| customize the process of booting the http server.
-|
-| """ Loading ace commands """
-|     At times you may want to load ace commands when starting the HTTP server.
-|     Same can be done by chaining `loadCommands()` method after
-|
-| """ Preloading files """
-|     Also you can preload files by calling `preLoad('path/to/file')` method.
-|     Make sure to pass a relative path from the project root.
-*/
-
 const { Ignitor } = require('@adonisjs/ignitor')
+const path = require('path')
 const https = require('https')
-const pem = require('pem')
+const fs = require('fs')
 
-pem.createCertificate({ days: 1, selfSigned: true }, (error, keys) => {
-  if (error) {
-    return console.log(error)
-  }
+// Certificate
+const options = {
+  key: fs.readFileSync(path.join(__dirname, './server.key')),
+  cert: fs.readFileSync(path.join(__dirname, './server.crt'))
+}
 
-  const options = {
-    key: keys.serviceKey,
-    cert: keys.certificate
-  }
-
-  new Ignitor(require('@adonisjs/fold'))
-    .appRoot(__dirname)
-    .fireHttpServer((handler) => {
-      return https.createServer(options, handler)
-    })
-    .catch(console.error)
-})
+new Ignitor(require('@adonisjs/fold'))
+  .appRoot(__dirname)
+  .fireHttpServer((handler) => {
+    return https.createServer(options, handler)
+  })
+  .catch(console.error)
