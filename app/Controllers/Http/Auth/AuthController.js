03a3c58a6b2e7e00587e5b462071e4b5f2470035
Harysson Soares <haryssonsoares@gmail.com>
2021-04-09 11:08:05 -0300

    refact: clean code

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -1,36 +1,32 @@
-'use strict'
+"use strict";
 
-const User = use("App/Models/User")
+const User = use("App/Models/User");
 
 class AuthController {
-
-    async auth({request, response, auth}) {
-        const {email, password} = request.all();
-        if (!email || !password) {
-            return response.status(400).json({
-                message: (!email) ? "Email is required" : "password is required"
-            })
-        }
-        try {
-
-            const token = await auth.attempt(email, password);
-
-            const user = await User.findBy("email", email)
-
-            return response.status(200).json({
-                message: "user logged",
-                token,
-                user 
-            })
-
-        } catch (err) {
-            return response.status(404).json({
-                message: "error during login",
-                error: err.code
-            })
-        }
+  async auth({ request, response, auth }) {
+    const { email, password } = request.all();
+    if (!email || !password) {
+      return response.status(400).json({
+        message: !email ? "Email is required" : "password is required",
+      });
     }
+    try {
+      const token = await auth.attempt(email, password);
 
+      const user = await User.findBy("email", email);
+
+      return response.status(200).json({
+        message: "user logged",
+        token,
+        user,
+      });
+    } catch (err) {
+      return response.status(404).json({
+        message: "error during login",
+        error: err.code,
+      });
+    }
+  }
 }
 
-module.exports = AuthController
+module.exports = AuthController;
