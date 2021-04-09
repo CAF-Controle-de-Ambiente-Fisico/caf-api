592f7d1b9d256c0d5f6f2cf27aa765d8d1856703
Harysson Soares <haryssonsoares@gmail.com>
2021-04-09 11:09:17 -0300

    style: email to confirm signup helpers

--- app/helpers/sendEmail.js
+++ app/helpers/sendEmail.js
@@ -5,22 +5,23 @@ const sgMail = use("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = async (data) => {
-    const msg = {
-        to: data.email, 
-        from: "caf.trt21@gmail.com", 
-        subject: "Confirmação de Senha",
-        text: `Link para confirmação de senha`,
-        html: `<h1>Olá ${data.username}</h1> 
+  const msg = {
+    to: data.email,
+    from: "caf.trt21@gmail.com",
+    subject: "Confirmação de Senha",
+    text: `Link para confirmação de senha`,
+    html: `<h1>Olá ${data.username}</h1> 
             <p>Clique no link abaixo para o próximo passo do seu cadastro:</p> 
-            <a href='http://localhost:3000/signup/confirmation?token=${data.code}&role=${data.role}'> Seguir com meu cadastro </a>`};
-    sgMail
+            <a target="_blank" href='http://localhost:3000/signup/confirmation?token=${data.code}&role=${data.role}'> Seguir com meu cadastro </a>`,
+  };
+  sgMail
     .send(msg)
-    .then(() => {   
-        console.log("Email sent");
+    .then(() => {
+      console.log("Email sent");
     })
     .catch((error) => {
-        console.error(error);
-      });
-  }
+      console.error(error);
+    });
+};
 
-module.exports = sendEmail; 
+module.exports = sendEmail;
